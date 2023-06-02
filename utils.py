c43db2aef49727aaf9ecaca8040ca2398f27f231
Navya-S <43130696+Navya-S@users.noreply.github.com>
2023-06-02 23:41:39 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,29 +1,83 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
+from tqdm import tqdm
+from torchvision import transforms
+from torchsummary import summary
 
+def train(model, device, train_loader, optimizer, criterion):
+  model.train()
+  pbar = tqdm(train_loader)
 
-# Train data transformations
-train_transforms = transforms.Compose([
-    transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
-    transforms.Resize((28, 28)),
-    transforms.RandomRotation((-15., 15.), fill=0),
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,)),
-    ])
+  train_loss = 0
+  correct = 0
+  processed = 0
 
-# Test data transformations
-test_transforms = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
-    ])
+  for batch_idx, (data, target) in enumerate(pbar):
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
 
-kwargs = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 2, 'pin_memory': True}
+    # Predict
+    pred = model(data)
 
-test_loader = torch.utils.data.DataLoader(test_data, **kwargs)
-train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
+    # Calculate loss
+    loss = criterion(pred, target)
+    train_loss+=loss.item()
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+    
+    correct += GetCorrectPredCount(pred, target)
+    processed += len(data)
+
+    pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+  train_acc = 100*correct/processed
+  train_losses = train_loss/len(train_loader)
+  return train_acc, train_losses
+
+  
+def test(model, device, test_loader, criterion):
+  model.eval()
+  test_loss = 0
+  correct = 0
+  with torch.no_grad():
+      for batch_idx, (data, target) in enumerate(test_loader):
+          data, target = data.to(device), target.to(device)
+          output = model(data)
+          test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+          correct += GetCorrectPredCount(output, target)
+  test_loss /= len(test_loader.dataset)
+  test_acc = 100. * correct / len(test_loader.dataset)
+
+  print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+      test_loss, correct, len(test_loader.dataset),
+      100. * correct / len(test_loader.dataset)))
+  return test_acc, test_loss
 
 def GetCorrectPredCount(pPrediction, pLabels):
   return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+def data_transforms():
+  # Train data transformations
+  train_transforms = transforms.Compose([
+      transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
+      transforms.Resize((28, 28)),
+      transforms.RandomRotation((-15., 15.), fill=0),
+      transforms.ToTensor(),
+      transforms.Normalize((0.1307,), (0.3081,)),
+      ])
+
+  # Test data transformations
+  test_transforms = transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize((0.1307,), (0.3081,))
+      ])
+  return train_transforms, test_transforms
+
+def model_summary(model, input_size):
+  use_cuda = torch.cuda.is_available()
+  device = torch.device("cuda" if use_cuda else "cpu")
+  model_summary = summary(model, input_size)
+  return model_summary 
