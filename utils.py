846f988f7bd4f4d6c66ece5e12d7b31cc66eeea7
Navya-S <43130696+Navya-S@users.noreply.github.com>
2023-06-02 21:35:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
 
+
+# Train data transformations
+train_transforms = transforms.Compose([
+    transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
+    transforms.Resize((28, 28)),
+    transforms.RandomRotation((-15., 15.), fill=0),
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,)),
+    ])
+
+# Test data transformations
+test_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+kwargs = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 2, 'pin_memory': True}
+
+test_loader = torch.utils.data.DataLoader(test_data, **kwargs)
+train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
+
+def GetCorrectPredCount(pPrediction, pLabels):
+  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
