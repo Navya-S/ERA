ad4d6c245a61e64b1ec86e911158f027d3be05a0
Navya-S <43130696+Navya-S@users.noreply.github.com>
2023-06-03 00:42:19 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from tqdm import tqdm
-from torchvision import transforms
 from torchsummary import summary
 
 def train(model, device, train_loader, optimizer, criterion):
@@ -59,22 +58,16 @@ def test(model, device, test_loader, criterion):
 def GetCorrectPredCount(pPrediction, pLabels):
   return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
 
-def data_transforms():
-  # Train data transformations
-  train_transforms = transforms.Compose([
-      transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
-      transforms.Resize((28, 28)),
-      transforms.RandomRotation((-15., 15.), fill=0),
-      transforms.ToTensor(),
-      transforms.Normalize((0.1307,), (0.3081,)),
-      ])
-
-  # Test data transformations
-  test_transforms = transforms.Compose([
-      transforms.ToTensor(),
-      transforms.Normalize((0.1307,), (0.3081,))
-      ])
-  return train_transforms, test_transforms
+def plot_metrics(train_losses, train_acc, test_losses, test_acc):
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  axs[0, 0].plot(train_losses)
+  axs[0, 0].set_title("Training Loss")
+  axs[1, 0].plot(train_acc)
+  axs[1, 0].set_title("Training Accuracy")
+  axs[0, 1].plot(test_losses)
+  axs[0, 1].set_title("Test Loss")
+  axs[1, 1].plot(test_acc)
+  axs[1, 1].set_title("Test Accuracy")
 
 def model_summary(model, input_size):
   use_cuda = torch.cuda.is_available()
